7563be3c8d520e7218c26a4e03ce213779e5b277
HijbullahMahmud <hijbullahmahmud14@gmail.com>
2021-04-09 00:32:33 +0600

    Sample Project

--- lib/src/utils/settings.dart
+++ lib/src/utils/settings.dart
@@ -1,3 +1,4 @@
 /// Define App ID and Token
-const APP_ID = Your_App_ID;
-const Token = Your_Token;
+const APP_ID = "57ec3304068449d5a4371697f4f3b2b4";
+const Token =
+    "00657ec3304068449d5a4371697f4f3b2b4IADIG+kiHFcm6GEs+KEzQz3WcjRbOh4k+2tMiNkJITVs+/Vzp/YAAAAAEAAVx5nnX51wYAEAAQBenXBg";
